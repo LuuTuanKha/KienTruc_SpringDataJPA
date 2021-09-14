8699b0b33d1912e8c683a8960acffac5f977dfda
LuuTuanKha <74186947+LuuTuanKha@users.noreply.github.com>
2021-09-14 17:17:58 +0700

    native query

--- src/main/java/com/iuh/luutkha/repo/StudentRepo.java
+++ src/main/java/com/iuh/luutkha/repo/StudentRepo.java
@@ -1,8 +1,26 @@
 package com.iuh.luutkha.repo;
 
+import java.util.Collection;
+
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import com.iuh.luutkha.entity.Student;
+@Repository
 public interface StudentRepo extends JpaRepository<User, Integer> {
+	@Query("SELECT s FROM students s")
+	Collection<Student> findAllStudents();
+
+	@Modifying
+	@Query(value = "delete from students s where s.name= :name", nativeQuery = true)
+	void deleteStudentsByName(@Param("name") String name);
+
+	@Modifying
+	@Query(value = "update students set name= :name where id = :id", nativeQuery = true)
+	void updateStudentName(String name, Integer id);
 
 }
